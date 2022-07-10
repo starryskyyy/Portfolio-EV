b61b8a8451b60d8636d35fccd7550fe391e22f51
Elizaveta Vygovskaia <90989893+starryskyyy@users.noreply.github.com>
2022-07-09 21:14:51 -0400

    Add files via upload

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,8 +1,3 @@
-$(document).ready(function () {
-    $(":text").keyup(function () {
-        $(".display").text($(":text").val());
-    });
-});
 
 function sendMail() {
     var params = {
@@ -10,7 +5,41 @@ function sendMail() {
         email_id : document.getElementById("email_id").value,
         message : document.getElementById("message").value
     }
-    emailjs.send("service_vqnoby5", "template_is1ti5m", params).then(function (res) {
-        alert("Success " + res.status);
+
+    emailjs.send("service_vqnoby5","template_is1ti5m", params).then(function (res) {
+        console.log("Success " + res.status);
     })
 }
+
+var submit_button = document.getElementById("btn-submit");
+
+submit_button.addEventListener("click", function(e) {
+    var required = document.querySelectorAll("input[required]");
+
+    required.forEach(function(element) {
+        if(element.value.trim() == "") {
+            element.style.borderColor = "#48101B";
+        } else {
+            element.style.backgroundColor = "#011321";
+        }
+    });
+});
+$(document).ready(function () {
+    $("#name").keyup(function () {
+        $(".name-code").text($("#name").val());
+    });
+    $("#email_id").keyup(function () {
+        $(".email-code").text($("#email_id").val());
+    });
+    $("#message").keyup(function () {
+        $(".message-code").text($("#message").val());
+    });
+});
+
+var monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+var dayNames= ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+
+var newDate = new Date();
+newDate.setDate(newDate.getDate() + 1);
+$('.date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()]);
